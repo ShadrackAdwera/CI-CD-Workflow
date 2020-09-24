7809b2fb6352b538e739577a8b1a3cf0c1fd0c60
FACTS  LIMITED <admin@factsafrica.com>
2020-09-24 10:11:08 +0300

    feat: ci workflow

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({adapter: new Adapter()})
+
+describe('App', ()=>{
+  it('renders without crashing', ()=>{
+    const wrapper = shallow(<App />)
+    expect(wrapper.contains(<div className='App'></div>))
+  })
+})
+
